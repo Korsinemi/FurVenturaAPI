2e96c4f57bdfc61e6afcbb02887dc6510e2f96f5
Furrycality <furrycality@gmail.com>
2024-08-23 17:36:00 -0500

    v1.0.1 - Leer el Changelog en README

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import bodyparser from 'body-parser';
+import cookieparser from 'cookie-parser'
+import morgan from 'morgan'
 import gameRoutes from './routes/gameRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsConfig = {
+    origin: "*",
+    credential: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+}
+app.use(morgan('dev'))
+app.options("", cors(corsConfig))
+app.use(cors(corsConfig))
 app.use(bodyparser.json());
+app.use(cookieparser());
 app.use('/api/game', gameRoutes);
 
 app.get('/', (req, res) => {
